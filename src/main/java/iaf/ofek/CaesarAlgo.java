ffb5dfcd361680d1d3e7fdadbd5de2123e8debff
dor4420 <dorsahar@icloud.com>
2023-05-28 19:34:11 +0300

    Add final variable

--- src/main/java/iaf/ofek/CaesarAlgo.java
+++ src/main/java/iaf/ofek/CaesarAlgo.java
@@ -5,13 +5,16 @@ import java.util.Random;
 
 public class CaesarAlgo {
 
-    private static final byte HIGHEST_KEY_ALLOWED = (byte) 127 - 31;
+    private static final byte HIGHEST_KEY_ALLOWED = 127;
+    private static final byte LOWEST_KEY_ALLOWED = 31;
+
+    private static final byte KEY_RANGE = HIGHEST_KEY_ALLOWED - LOWEST_KEY_ALLOWED;
 
     public static String encrypt(String message, boolean isTest) {
         byte[] messageInBytes = message.getBytes();
         byte[] encryptedData = new byte[messageInBytes.length];
 
-        byte key = (byte) new Random().nextInt(HIGHEST_KEY_ALLOWED);
+        byte key = (byte) new Random().nextInt(KEY_RANGE);
 
         if (isTest) {
             key = (byte) 87;
@@ -22,8 +25,8 @@ public class CaesarAlgo {
         System.out.println("Encrypting...");
         for (int i = 0; i < messageInBytes.length; i++) {
             int sum = messageInBytes[i] + key;
-            if (sum >= 127) {
-                sum -= HIGHEST_KEY_ALLOWED; //127 = HIGHEST DEC NUMBER ALLOWED  31 = LOWEST ALLOWED
+            if (sum >= HIGHEST_KEY_ALLOWED) {
+                sum -= KEY_RANGE; //127 = HIGHEST DEC NUMBER ALLOWED  31 = LOWEST ALLOWED
             }
             encryptedData[i] = (byte) sum;
         }
@@ -36,8 +39,8 @@ public class CaesarAlgo {
         System.out.println("Decrypting...");
         for (int i = 0; i < messageInBytes.length; i++) {
             int sum = messageInBytes[i] - key;
-            if (sum <= 31) {
-                sum += HIGHEST_KEY_ALLOWED;
+            if (sum <= LOWEST_KEY_ALLOWED) {
+                sum += KEY_RANGE;
             }
             decryptedData[i] = (byte) sum;
         }
