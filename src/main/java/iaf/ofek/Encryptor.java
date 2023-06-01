ba6777e2b8f7f839ae56b40864a727666b36505b
dor4420 <dorsahar@icloud.com>
2023-06-01 22:56:25 +0300

    Add Encryptor, works with the FileHelper and calls the DataManipulator

---
+++ src/main/java/iaf/ofek/Encryptor.java
@@ -0,0 +1,46 @@
+package iaf.ofek;
+
+import java.nio.file.Path;
+
+public class Encryptor {
+
+    private final FileHelper fileHelper;
+    private final byte key;
+
+    public Encryptor(String filePath, byte key) {
+        this.fileHelper = new FileHelper(filePath);
+        this.key = key;
+    }
+
+    public Encryptor(FileHelper fileHelper) {
+        this.fileHelper = fileHelper;
+        this.key = 0;
+    }
+
+
+    public void encrypt() {
+        byte[] data = fileHelper.readFromFile();
+        byte[] encryptedData;
+
+        encryptedData = ((key != 0) ? DataManipulator.encrypt(data, key) : DataManipulator.encrypt(data));
+
+        String encryptedFilePath = fileHelper.getFile().getPath() + ".encrypted";
+        FileHelper encryptedFile = new FileHelper(encryptedFilePath);
+
+        encryptedFile.createFile(encryptedData);
+    }
+
+    public void decrypt(byte key) {
+        byte[] data = fileHelper.readFromFile();
+        byte[] decryptedData = DataManipulator.decrypt(data, key);
+
+        Path filePath = fileHelper.getFile().toPath();
+        String decryptedFileDirPath = filePath.getParent().toString();
+        String fileName = fileHelper.getDecryptedName();
+        String decryptedFilePath = decryptedFileDirPath + "/" + fileName;
+
+        FileHelper decryptedFile = new FileHelper(decryptedFilePath);
+        
+        decryptedFile.createFile(decryptedData);
+    }
+}
